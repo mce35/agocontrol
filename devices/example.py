af6bcedfe23b41c3962d7036e662599477ecf652
Jeroen Simonetti <jeroen@simonetti.nl>
2014-04-06 21:51:54 +0200

    Fix style in example

--- devices/example.py
+++ devices/example.py
@@ -1,4 +1,5 @@
-#
+"""Agoclient example."""
+
 # ago client library example device
 #
 # copyright (c) 2013 Harald Klein <hari+ago@vt100.at>
@@ -8,85 +9,122 @@
 
 import agoclient
 
-# we'll also use a background thread in this example
+# we'll also use a BACKGROUND thread in this example
 
 import threading
 import time
 
 
-# then we create an AgoConnection object. You need to specify an instance name as parameter. 
-# This will be used to uniquely name the mappings file for internal id to uuid mapping and to identify the client devices ("handled-by" parameter)
-# Examples for the instance name would be "zwave" for our z-wave device, "knx" for KNX, "owfs" for the one wire device, and so on.
-# Just chose a name that is unique and speaks for itself. It is best practice to also use that instance name as section name in the config file
+# then we create an AgoConnection object.
+# You need to specify an instance name as parameter.
+# This will be used to uniquely name the mappings file
+# for internal id to uuid mapping and to identify the
+# CLIENT devices ("handled-by" parameter)
+# Examples for the instance name would be "zwave" for
+# our z-wave device, "knx" for KNX, "owfs" for the one
+# wire device, and so on.
+# Just chose a name that is unique and speaks for itself.
+# It is best practice to also use that instance name as
+# section name in the config file
 # if you need any config parameters.
 
-client = agoclient.AgoConnection("example")
+CLIENT = agoclient.AgoConnection("example")
 
 
-# the messageHandler method will be called by the client library when a message comes in that is destined for one of the child devices you're handling
-# the first parameter is your internal id (all the mapping from ago control uuids to the internal ids is handled transparently for you)
+# the message_handler method will be called by the CLIENT
+# library when a message comes in that is destined for
+# one of the child devices you're handling
+# the first parameter is your internal id (all the mapping
+# from ago control uuids to the internal ids is handled
+# transparently for you)
 # the second parameter is a dict with the message content
 
-def messageHandler(internalid, content):
-	if "command" in content:
-		if content["command"] == "on":
-			print "switching on: " + internalid
+def message_handler(internalid, content):
+    """The messagehandler."""
+    if "command" in content:
+        if content["command"] == "on":
+            print "switching on: " + internalid
 
-			# TODO: insert real stuff here, this is where you would do action and switch on your child device
+            # TODO: insert real stuff here, this
+                        # is where you would do action and
+                        # switch on your child device
+            # depending on if the operation was
+                        # successful, you want to report state
+                        # back to ago control. We just send
+                        # 255 in this case so that ago control
+                        # changes the device state in the
+                        # inventory to on
 
-			# depending on if the operation was successful, you want to report state back to ago control. We just send 255 in this case 
-			# so that ago control changes the device state in the inventory to on
+            CLIENT.emit_event(internalid,
+                "event.device.statechanged", "255", "")
 
-			client.emit_event(internalid, "event.device.statechanged", "255", "")
-
-		if content["command"] == "off":
-			print "switching off: " + internalid
-			client.emit_event(internalid, "event.device.statechanged", "0", "")
+        if content["command"] == "off":
+            print "switching off: " + internalid
+            CLIENT.emit_event(internalid, "event.device.statechanged", "0", "")
 
 # specify our message handler method
-client.add_handler(messageHandler)
+CLIENT.add_handler(message_handler)
 
 
-# if you need to fetch any settings from config.ini, use the get_config_option call. The first parameter is the section name in the file (should be yor instance name)
-# the second one is the parameter name, and the third one is the default value for the case when nothing is set in the config.ini
+# if you need to fetch any settings from config.ini,
+# use the get_config_option call. The first parameter
+# is the section name in the file (should be your instance name)
+# the second one is the parameter name, and the third one
+# is the default value for the case when nothing is set in the config.ini
 
 print agoclient.get_config_option("example", "parameter", "0")
 
 
-# of course you need to tell the client library about the devices you provide. The add_device call expects a internal id and a device type (you can find all valid types
-# in the schema.yaml configuration file). The internal id is whatever you're using in your code to distinct your devices. Or the pin number of some GPIO output. Or
-# the IP of a networked device. Whatever fits your device specific stuff. The persistent translation to a ago control uuid will be done by the client library. The
-# mapping is stored as a json file in CONFDIR/uuidmap/<instance name>.json
-# you don't need to worry at all about this, when the messageHandler is called, you'll be passed the internalid for the device that you did specifiy when using add_device()
+# of course you need to tell the CLIENT library about the
+# devices you provide. The add_device call expects a internal id
+# and a device type (you can find all valid types in the
+# schema.yaml configuration file). The internal id is whatever
+# you're using in your code to distinct your devices.
+# Or the pin number of some GPIO output. Or the IP of a
+# networked device. Whatever fits your device specific stuff.
+# The persistent translation to a ago control uuid will be
+# done by the CLIENT library. The mapping is stored as a json
+# file in CONFDIR/uuidmap/<instance name>.json
+# you don't need to worry at all about this, when the
+# message_handler is called, you'll be passed the internalid
+# for the device that you did specifiy when using add_device()
 
-# we add a switch and a dimmer 
-client.add_device("123", "dimmer")
-client.add_device("124", "switch")
+# we add a switch and a dimmer
+CLIENT.add_device("123", "dimmer")
+CLIENT.add_device("124", "switch")
 # for our threading example in the next section we also add a binary sensor:
-client.add_device("125", "binarysensor")
+CLIENT.add_device("125", "binarysensor")
 
-# then we add a background thread. This is not required and just shows how to send events from a separate thread. This might be handy when you have to poll something
-# in the background or need to handle some other communication. If you don't need one or if you want to keep things simple at the moment just skip this section.
+# then we add a BACKGROUND thread. This is not required and
+# just shows how to send events from a separate thread. This might
+# be handy when you have to poll something in the BACKGROUND or need
+# to handle some other communication. If you don't need one or if
+# you want to keep things simple at the moment just skip this section.
 
-class testEvent(threading.Thread):
+
+class TestEvent(threading.Thread):
+    """Test Event."""
     def __init__(self,):
-        threading.Thread.__init__(self)    
+        threading.Thread.__init__(self)
+
     def run(self):
-    	level = 0
+        level = 0
         while (True):
-			client.emit_event("125", "event.security.sensortriggered", level, "")
-			if (level == 0):
-				level = 255
-			else:
-				level = 0
-			time.sleep (5)
-      
-background = testEvent()
-background.setDaemon(True)
-background.start()
+            CLIENT.emit_event("125",
+                "event.security.sensortriggered", level, "")
+            if (level == 0):
+                level = 255
+            else:
+                level = 0
+            time.sleep(5)
+
+BACKGROUND = TestEvent()
+BACKGROUND.setDaemon(True)
+BACKGROUND.start()
 
 
-# now you should have added all devices, set up all your internal and device specific stuff, started everything like listener threads or whatever. The call to run()
-# is blocking and will start the message handling
-client.run()
-
+# now you should have added all devices, set up all your internal
+# and device specific stuff, started everything like listener threads
+# or whatever. The call to run() is blocking and
+# will start the message handling
+CLIENT.run()
