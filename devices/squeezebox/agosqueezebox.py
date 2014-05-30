8a982a74d0d8cdfdb1bbed5f659ca94d6cfd1607
tang <tang@tanguy.no-ip.org>
2014-05-30 09:42:13 +0200

    Fix some issues on squeezebox

--- devices/squeezebox/agosqueezebox.py
+++ devices/squeezebox/agosqueezebox.py
@@ -18,15 +18,18 @@ playlist = None
 library = None
 client = None
 states = {}
-mediastates = {}
 
 STATE_OFF = 0
 STATE_ON = 255
+STATE_STREAM = 50
+STATE_PLAY = 100
+STATE_STOP = 150
+STATE_PAUSE = 200
 
-STATE_STREAM = 'streaming'
-STATE_PLAY = 'play'
-STATE_STOP = 'stopped'
-STATE_PAUSE = 'paused'
+MEDIASTATE_STREAM = 'streaming'
+MEDIASTATE_PLAY = 'play'
+MEDIASTATE_STOP = 'stopped'
+MEDIASTATE_PAUSE = 'paused'
 
 #logging.basicConfig(filename='agosqueezebox.log', level=logging.INFO, format="%(asctime)s %(levelname)s : %(message)s")
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(name)s %(levelname)s : %(message)s")
@@ -55,50 +58,57 @@ def quit(msg):
 #squeezebox callbacks
 def play_callback(player_id, track_title, playlist_index):
     #if player is off or player is already playing, kick this callback
-    logging.info('callback PLAY: %s' % player_id)
-    if mediastates.has_key(player_id) and mediastates[player_id]!=STATE_PLAY and mediastates[player_id]!=STATE_STREAM:
-        mediastates[player_id] = STATE_PLAY
+    logging.debug('callback PLAY: %s' % player_id)
+    if states.has_key(player_id) and states[player_id]!=STATE_PLAY and states[player_id]!=STATE_OFF and states[player_id]!=STATE_STREAM:
+        states[player_id] = STATE_PLAY
         emit_play(player_id)
 def stop_callback(player_id):
     #if player is off, kick this callback
-    logging.info('callback STOP: %s' % player_id)
-    if mediastates.has_key(player_id) and mediastates[player_id]!=STATE_STREAM:
-        mediastates[player_id] = STATE_STOP
+    logging.debug('callback STOP: %s' % player_id)
+    if states.has_key(player_id) and states[player_id]!=STATE_STOP and states[player_id]!=STATE_OFF and states[player_id]!=STATE_STREAM:
+        states[player_id] = STATE_STOP
         emit_stop(player_id)
 def pause_callback(player_id):
     #if player is off, kick this callback
-    logging.info('callback PAUSE: %s' % player_id)
-    if mediastates.has_key(player_id) and mediastates[player_id]!=STATE_STREAM:
-        mediastates[player_id] = STATE_PAUSE
+    logging.debug('callback PAUSE: %s' % player_id)
+    if states.has_key(player_id) and states[player_id]!=STATE_PAUSE and states[player_id]!=STATE_OFF and states[player_id]!=STATE_STREAM:
+        states[player_id] = STATE_PAUSE
         emit_pause(player_id)
 def on_callback(player_id):
-    logging.info('callback ON: %s' % player_id)
-    if mediastates.has_key(player_id) and mediastates[player_id]!=STATE_STREAM:
+    logging.debug('callback ON: %s' % player_id)
+    if states.has_key(player_id) and states[player_id]!=STATE_ON and states[player_id]!=STATE_STREAM:
         states[player_id] = STATE_ON
         emit_on(player_id)
 def off_callback(player_id):
-    logging.info('callback OFF: %s' % player_id)
-    if mediastates.has_key(player_id) and mediastates[player_id]!=STATE_STREAM:
+    logging.debug('callback OFF: %s' % player_id)
+    if states.has_key(player_id) and states[player_id]!=STATE_OFF and states[player_id]!=STATE_STREAM:
         states[player_id] = STATE_OFF
         emit_off(player_id)
 
 
 #emit function
 def emit_play(internalid):
-    client.emit_event(internalid, "event.mediaplayer.statechanged", "play", "")
-    client.emit_event(internalid, "event.device.mediastatechanged", str(STATE_PLAY), "")
+    logging.debug('emit PLAY')
+    client.emit_event(internalid, "event.device.statechanged", str(STATE_PLAY), "")
+    client.emit_event(internalid, "event.device.mediastatechanged", str(MEDIASTATE_PLAY), "")
 def emit_stop(internalid):
-    client.emit_event(internalid, "event.mediaplayer.statechanged", "stop", "")
-    client.emit_event(internalid, "event.device.mediastatechanged", str(STATE_STOP), "")
+    logging.debug('emit STOP')
+    client.emit_event(internalid, "event.device.statechanged", str(STATE_STOP), "")
+    client.emit_event(internalid, "event.device.mediastatechanged", str(MEDIASTATE_STOP), "")
 def emit_pause(internalid):
-    client.emit_event(internalid, "event.mediaplayer.statechanged", "pause", "")
-    client.emit_event(internalid, "event.device.mediastatechanged", str(STATE_PAUSE), "")
+    logging.debug('emit PAUSE')
+    client.emit_event(internalid, "event.device.statechanged", str(STATE_PAUSE), "")
+    client.emit_event(internalid, "event.device.mediastatechanged", str(MEDIASTATE_PAUSE), "")
 def emit_on(internalid):
+    logging.debug('emit ON')
     client.emit_event(internalid, "event.device.statechanged", str(STATE_ON), "")
 def emit_off(internalid):
+    logging.debug('emit OFF')
     client.emit_event(internalid, "event.device.statechanged", str(STATE_OFF), "")
 def emit_stream(internalid):
-    client.emit_event(internalid, "event.device.mediastatechanged", str(STATE_STREAM), "")
+    logging.debug('emit STREAM')
+    client.emit_event(internalid, "event.device.statechanged", str(STATE_STREAM), "")
+    client.emit_event(internalid, "event.device.mediastatechanged", str(MEDIASTATE_STREAM), "")
 
 #init
 try:
@@ -216,30 +226,27 @@ try:
         if p.get_model()=='http':
             #it's a stream. No control on it
             logging.info('  Player %s[%s] is a streaming' % (p.name, p.mac))
-            states[p.mac] = STATE_ON
-            mediastates[p.mac] = STATE_STREAM
+            states[p.mac] = STATE_STREAM
             emit_stream(p.mac)
         elif not p.get_is_on():
             #player is off
             logging.info('  Player %s[%s] is off' % (p.name, p.mac))
-            states[p.mac] = STATE_OFF
-            mediastates[p.mac] = STATE_STOP
+            states[p.mac] = STATE_STOP
             emit_off(p.mac)
         else:
             #player is on
             mode = p.get_mode()
-            states[p.mac] = STATE_ON
             if mode=='stop':
                 logging.info('  Player %s[%s] is stopped' % (p.name, p.mac))
-                mediastates[p.mac] = STATE_STOP
+                states[p.mac] = STATE_STOP
                 emit_stop(p.mac)
             elif mode=='play':
                 logging.info('  Player %s[%s] is playing' % (p.name, p.mac))
-                mediastates[p.mac] = STATE_PLAY
+                states[p.mac] = STATE_PLAY
                 emit_play(p.mac)
             elif mode=='pause':
                 logging.info('  Player %s[%s] is paused' % (p.name, p.mac))
-                mediastates[p.mac] = STATE_PAUSE
+                states[p.mac] = STATE_PAUSE
                 emit_pause(p.mac)
 except Exception as e:
     quit('Failed to get player status. Exit now. (%s)' % str(e))
