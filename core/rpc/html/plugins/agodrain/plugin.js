5858dd361d5e36660df562575105a676aaaf06e4
Tang <tanguy.bonneau+agocontrol@gmail.com>
2014-07-13 16:16:49 +0200

    Add clear command to remove existing entries

--- core/rpc/html/plugins/agodrain/plugin.js
+++ core/rpc/html/plugins/agodrain/plugin.js
@@ -10,32 +10,36 @@ function agodrainPlugin() {
     var subDisplayed = false;
 
     this.displayEvent = function(response) {
-	if (response.error) {
-		return;
-	} else {
-		if (response.result) { 
-			var output = document.getElementById('output');
-			var content = JSON.stringify(response.result);
-			output.innerHTML = '<li class="default alert">' + content + '</li>' + output.innerHTML;
-		}
-		getEvent();
-	}
+        if (response.error) {
+            return;
+        } else {
+            if (response.result) { 
+                var output = document.getElementById('output');
+                var content = JSON.stringify(response.result);
+                output.innerHTML = '<li class="default alert">' + content + '</li>' + output.innerHTML;
+            }
+            getEvent();
+        }
     };
 
     this.stopDrain = function() {
         handleEvent = _originalHandler;
     };
 
+    this.clear = function() {
+        var output = $('#output');
+        output.empty();
+    }
+
     this.startDrain = function() {
-	if (!self.subDisplayed) {
-	    var output = document.getElementById('output');
-	    output.innerHTML = '<br>';
-	    output.innerHTML = output.innerHTML + '<li class="success alert">Client subscription uuid: ' + subscription + '</li>';
-	    self.subDisplayed = true;
-	}
-	
-	_originalHandler = handleEvent;
-	handleEvent = self.displayEvent;
+        if (!self.subDisplayed) {
+            var output = document.getElementById('output');
+            output.innerHTML = '<br>';
+            output.innerHTML = output.innerHTML + '<li class="success alert">Client subscription uuid: ' + subscription + '</li>';
+            self.subDisplayed = true;
+        }
+        _originalHandler = handleEvent;
+        handleEvent = self.displayEvent;
     };
 }
 
@@ -43,7 +47,7 @@ function init_plugin()
 {
     model = new agodrainPlugin();
     model.mainTemplate = function() {
-	return templatePath + "agodrain";
+        return templatePath + "agodrain";
     }.bind(model);
 
     return model;
