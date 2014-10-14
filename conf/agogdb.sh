d237cfc5d7a2c117bd2d1bcc851c0bfadc087b20
Harald Klein <hari@vt100.at>
2014-10-14 23:07:24 +0200

    remove tempfile

--- conf/agogdb.sh
+++ conf/agogdb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 FILENAME="$1"
+test -e "$FILENAME" || exit
 TMPFILE=$(mktemp)
 PROGRAM=$(bzgrep '^Program:' "$FILENAME" | cut -d ":" -f 2)
 bzcat "$FILENAME" | uudecode > "$TMPFILE"
 echo $PROGRAM
 echo "thr a a bt" | gdb -q "$PROGRAM" "$TMPFILE" 
+rm "$TMPFILE"
 
