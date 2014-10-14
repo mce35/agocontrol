a9e34cb4472a6b12a8e34c3ff838e6d50e295b1d
Harald Klein <hari@vt100.at>
2014-10-14 23:59:09 +0200

    use inotify

--- conf/agogdb.sh
+++ conf/agogdb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-FILENAME="$1"
-test -e "$FILENAME" || exit
-TMPFILE=$(mktemp)
-PROGRAM=$(bzgrep '^Program:' "$FILENAME" | cut -d ":" -f 2)
-bzcat "$FILENAME" | uudecode > "$TMPFILE"
-echo $PROGRAM
-echo "thr a a bt" | gdb -q "$PROGRAM" "$TMPFILE" 
-rm "$TMPFILE"
-
+DIR=/var/crash
+while RES=$(inotifywait -e create $DIR); do
+	FILENAME=${RES#?*CREATE }
+	echo $FILENAME
+	TMPFILE=$(mktemp)
+	PROGRAM=$(bzgrep '^Program:' "$FILENAME" | cut -d ":" -f 2)
+	bzcat "$FILENAME" | uudecode > "$TMPFILE"
+	echo $PROGRAM
+	echo "thr a a bt" | gdb -q "$PROGRAM" "$TMPFILE"
+	rm "$TMPFILE"
+done
