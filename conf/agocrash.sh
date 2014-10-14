c41bdb8a80047d9385663e4c2431b0852bee8671
Harald Klein <hari@vt100.at>
2014-10-14 22:34:13 +0200

    add description

--- conf/agocrash.sh
+++ conf/agocrash.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# writes a bzip compressed file with program name, and a uuencoded core dump
+# use a kernel core_pattern like this: |/var/crash/agocrash.sh %p %s %c %E
+#
+
 PROGRAM=$(echo $4 | sed 's|!|/|g')
 BASENAME=$(basename "$PROGRAM")
 FILENAME="/var/crash/$BASENAME-$1.crash.bz2"
