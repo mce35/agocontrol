ecaf9e08cd11ec7a2cd7034a36b022308318f2b4
Harald Klein <hari@vt100.at>
2014-10-16 11:13:07 +0200

    split core and queue file

--- conf/agocrash.sh
+++ conf/agocrash.sh
@@ -3,13 +3,16 @@
 # writes a bzip compressed file with program name, and a uuencoded core dump
 # use a kernel core_pattern like this: |/var/crash/agocrash.sh %p %s %c %E
 #
-
+BASEDIR=/var/crash
+QUEUEDIR=${BASEDIR}/queue
 PROGRAM=$(echo $4 | sed 's|!|/|g')
 BASENAME=$(basename "$PROGRAM")
-FILENAME="/var/crash/$BASENAME-$1.crash.bz2"
+COREFILENAME="${BASEDIR}/$1.core"
+QUEUFILENAME="${QUEUEDIR}/$1"
+
+cat - > ${COREFILENAME}
+
 (
 	echo "Pid:$1"
 	echo "Program:$PROGRAM"
-	echo "Core:"
-	cat - | uuencode -m -
-) | bzip2 > "$FILENAME"
+)  > "$QUEUEFILENAME"
