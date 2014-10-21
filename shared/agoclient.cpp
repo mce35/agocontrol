c1ad6e87269cee2354d6caa806491fb46a7b33df
Johan Ström <johan@stromnet.se>
2014-10-21 13:08:24 +0200

    Cleaned out old Log class (barely/never used?)

--- shared/agoclient.cpp
+++ shared/agoclient.cpp
@@ -1059,36 +1059,3 @@ bool agocontrol::AgoConnection::setGlobalVariable(std::string variable, qpid::ty
 	return false;
 }
 
-agocontrol::Log::Log(std::string ident, int facility) {
-	facility_ = facility;
-	priority_ = LOG_DEBUG;
-	strncpy(ident_, ident.c_str(), sizeof(ident_));
-	ident_[sizeof(ident_)-1] = '\0';
-
-	openlog(ident_, LOG_PID, facility_);
-}
-
-int agocontrol::Log::sync() {
-	if (buffer_.length()) {
-		syslog(priority_, "%s", buffer_.substr(0,buffer_.length()-1).c_str());
-		buffer_.erase();
-		priority_ = LOG_DEBUG; // default to debug for each message
-	}
-	return 0;
-}
-
-int agocontrol::Log::overflow(int c) {
-	if (c != EOF) {
-		buffer_ += static_cast<char>(c);
-	} else {
-		sync();
-	}
-	return c;
-}
-
-std::ostream& agocontrol::operator<< (std::ostream& os, const agocontrol::LogPriority& log_priority) {
-	static_cast<agocontrol::Log *>(os.rdbuf())->priority_ = (int)log_priority;
-	return os;
-}
-
-
