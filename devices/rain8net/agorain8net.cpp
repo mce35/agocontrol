3641f9cfff0ed7a1168d7f36c23172184de830b8
Harald Klein <hari@vt100.at>
2014-11-18 15:54:27 +0100

    appify rain8net

--- devices/rain8net/agorain8net.cpp
+++ devices/rain8net/agorain8net.cpp
@@ -23,16 +23,25 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#include "agoclient.h"
+#include "agoapp.h"
 
 #include "rain8.h"
-rain8net rain8;
-int rc;
 
 using namespace std;
 using namespace agocontrol;
 
-qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) {
+class AgoRain8net: public AgoApp {
+private:
+    rain8net rain8;
+    int rc;
+
+    void setupApp();
+    qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content);
+public:
+    AGOAPP_CONSTRUCTOR(AgoRain8net);
+};
+
+qpid::types::Variant::Map AgoRain8net::commandHandler(qpid::types::Variant::Map content) {
     qpid::types::Variant::Map returnval;
     int valve = 0;
     valve = atoi(content["internalid"].asString().c_str());
@@ -54,41 +63,39 @@ qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) {
     return returnval;
 }
 
-int main(int argc, char **argv) {
+void AgoRain8net::setupApp() {
     std::string devicefile;
 
     devicefile=getConfigOption("rain8net", "device", "/dev/ttyS_01");
 
     if (rain8.init(devicefile.c_str()) != 0) {
         AGO_FATAL() << "can't open rainnet device " << devicefile;
-        exit(1);
+        throw StartupError();
     }
     rain8.setTimeout(10000);
     if ((rc = rain8.comCheck()) != 0) {
         AGO_FATAL() << "can't talk to rainnet device " << devicefile << " - comcheck failed: " << rc;
-        exit(1);
+        throw StartupError();
     }
     AGO_INFO() << "connection to rain8net established";
-
-    AgoConnection agoConnection = AgoConnection("rain8net");		
 
     for (int i=1; i<9; i++) {
         std::stringstream valve;
         valve << i;
-        agoConnection.addDevice(valve.str().c_str(), "switch");
+        agoConnection->addDevice(valve.str().c_str(), "switch");
     }
-    agoConnection.addHandler(commandHandler);
+    addCommandHandler();
 
     AGO_DEBUG() << "fetching zone status";
     unsigned char status;
     if (rain8.getStatus(1,status) == 0) {
         AGO_FATAL() << "can't get zone status, aborting";
-        exit(1);
+        throw StartupError();
     } else {
         AGO_INFO() << "Zone status: 8:" << ((status & 128)?1:0) << " 7:" << ((status & 64)?1:0) << " 6:" << ((status &32)?1:0) << " 5:" 
             << ((status&16)?1:0) << " 4:" << ((status&8)?1:0) << " 3:" << ((status&4)?1:0) << " 2:" << ((status&2)?1:0) << " 1:" << ((status&1)?1:0);
     }
-    agoConnection.run();
 
 }
 
+AGOAPP_ENTRY_POINT(AgoRain8net);
