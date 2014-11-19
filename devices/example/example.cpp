3ac105bf318b0a709e4adebdbba2b437407f37c1
Johan Ström <johan@stromnet.se>
2014-11-19 23:16:40 +0100

    Update example C++ app with some more features, and build it so we catch errors

--- devices/example/example.cpp
+++ devices/example/example.cpp
@@ -1,11 +1,13 @@
 #include "agoapp.h"
 
 using namespace agocontrol;
+namespace po = boost::program_options;
 
 class AgoExample: public AgoApp {
 private:
     void setupApp();
     qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map command);
+    void appCmdLineOptions(boost::program_options::options_description &options);
 public:
     AGOAPP_CONSTRUCTOR(AgoExample);
 };
@@ -26,11 +28,35 @@ qpid::types::Variant::Map AgoExample::commandHandler(qpid::types::Variant::Map c
     return returnval;
 }
 
+void AgoExample::appCmdLineOptions(boost::program_options::options_description &options) {
+    options.add_options()
+        ("test,T", po::bool_switch(),
+         "A custom option which can be set or not set")
+        ("test-string", po::value<std::string>(),
+         "This can be a string")
+        ;
+}
+
 void AgoExample::setupApp() {
     AGO_INFO() << "example device starting up";
 
     // do some device specific setup
-    if (false) {
+    std::string test_param;
+
+    if (cli_vars.count("test-string"))
+        test_param = cli_vars["test-string"].as<std::string>();
+
+    AGO_INFO() << "Param Test string is " << test_param;
+
+    std::string cfgvalue = getConfigOption("test_option", "not set");
+    AGO_INFO() << "Config test_option is '" << cfgvalue << "'";
+
+    if(cfgvalue == "invalid") {
+        throw ConfigurationError("test-string must be set to 'valid'");
+    }
+
+    if(false) {
+        // Other startup failure
         AGO_FATAL() << "setup failed";
         throw StartupError();
     }
@@ -43,4 +69,7 @@ void AgoExample::setupApp() {
     addCommandHandler();
 }
 
+/* Finally create an application main entry point, this will create
+ * the int main(...) {} part */
 AGOAPP_ENTRY_POINT(AgoExample);
+
