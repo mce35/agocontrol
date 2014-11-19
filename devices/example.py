8a6165ad0a67d6f8623d6be5a442d766c0ff6904
Johan Ström <johan@stromnet.se>
2014-11-19 21:26:23 +0100

    Updated example.py, appified

--- devices/example.py
+++ devices/example.py
@@ -15,102 +15,151 @@ import threading
 import time
 
 
-# then we create an AgoConnection object.
-# You need to specify an instance name as parameter.
-# This will be used to uniquely name the mappings file
-# for internal id to uuid mapping and to identify the
-# CLIENT devices ("handled-by" parameter)
+# Every app should have one main class, which extends
+# the AgoApp class.
+# This provides boiler plate code common for all applications.
+#
+# The class name should be Ago<CLIENT>, and will be used
+# to uniquely name the mappings file for internal id-to-uuid
+# mapping, and to identify the CLIENT devices ("handled-by" parameter)
+#
 # Examples for the instance name would be "zwave" for
 # our z-wave device, "knx" for KNX, "owfs" for the one
 # wire device, and so on.
+#
 # Just chose a name that is unique and speaks for itself.
 # It is best practice to also use that instance name as
-# section name in the config file
-# if you need any config parameters.
-
-CLIENT = agoclient.AgoConnection("example")
-
-
-# the message_handler method will be called by the CLIENT
-# library when a message comes in that is destined for
-# one of the child devices you're handling
-# the first parameter is your internal id (all the mapping
-# from ago control uuids to the internal ids is handled
-# transparently for you)
-# the second parameter is a dict with the message content
-
-def message_handler(internalid, content):
-    """The messagehandler."""
-    if "command" in content:
-        if content["command"] == "on":
-            print "switching on: " + internalid
-
-            # TODO: insert real stuff here, this
-                        # is where you would do action and
-                        # switch on your child device
-            # depending on if the operation was
-                        # successful, you want to report state
-                        # back to ago control. We just send
-                        # 255 in this case so that ago control
-                        # changes the device state in the
-                        # inventory to on
-
-            CLIENT.emit_event(internalid,
-                "event.device.statechanged", "255", "")
-
-        if content["command"] == "off":
-            print "switching off: " + internalid
-            CLIENT.emit_event(internalid, "event.device.statechanged", "0", "")
-
-# specify our message handler method
-CLIENT.add_handler(message_handler)
+# section name in the config file if you need any config parameters.
+#
+# When developing, it is recommended to run with -d or -t argument,
+# to enable debug/trace logging and log to console.
+# Please refer to -h for other options.
+#
+class AgoExample(agoclient.AgoApp):
+    """AgoControl device with support for the Example protocol"""
+    # If your application has any custom command line parameters, implement
+    # the app_cmd_line_options method. Else, skip it
+    def app_cmd_line_options(self, parser):
+        """App-specific command line options"""
+        parser.add_argument('-T', '--test', action="store_true",
+                help="This can be set or not set")
 
 
-# if you need to fetch any settings from config.ini,
-# use the get_config_option call. The first parameter
-# is the section name in the file (should be your instance name)
-# the second one is the parameter name, and the third one
-# is the default value for the case when nothing is set in the config.ini
+    # the message_handler method will be called by the CLIENT
+    # library when a message comes in that is destined for
+    # one of the child devices you're handling
+    # the first parameter is your internal id (all the mapping
+    # from ago control uuids to the internal ids is handled
+    # transparently for you)
+    # the second parameter is a dict with the message content
+    def message_handler(self, internalid, content):
+        """The messagehandler."""
+        if "command" in content:
+            if content["command"] == "on":
+                # If we want to log state, we shall use self.log
+                # This supports multiple levels:
+                #   trace, debug, info, warning, error, fatal
+                #
+                self.log.debug("switching on: %s", internalid)
 
-print agoclient.get_config_option("example", "parameter", "0")
+                # TODO: insert real stuff here, this
+                            # is where you would do action and
+                            # switch on your child device
+                # depending on if the operation was
+                            # successful, you want to report state
+                            # back to ago control. We just send
+                            # 255 in this case so that ago control
+                            # changes the device state in the
+                            # inventory to on
 
+                # Emit an event which notifies all other components of the state change
+                self.connection.emit_event(internalid,
+                    "event.device.statechanged", "255", "")
 
-# of course you need to tell the CLIENT library about the
-# devices you provide. The add_device call expects a internal id
-# and a device type (you can find all valid types in the
-# schema.yaml configuration file). The internal id is whatever
-# you're using in your code to distinct your devices.
-# Or the pin number of some GPIO output. Or the IP of a
-# networked device. Whatever fits your device specific stuff.
-# The persistent translation to a ago control uuid will be
-# done by the CLIENT library. The mapping is stored as a json
-# file in CONFDIR/uuidmap/<instance name>.json
-# you don't need to worry at all about this, when the
-# message_handler is called, you'll be passed the internalid
-# for the device that you did specifiy when using add_device()
+            elif content["command"] == "off":
+                self.log.debug("switching off: %s", internalid)
+                self.connection.emit_event(internalid, "event.device.statechanged", "0", "")
 
-# we add a switch and a dimmer
-CLIENT.add_device("123", "dimmer")
-CLIENT.add_device("124", "switch")
-# for our threading example in the next section we also add a binary sensor:
-CLIENT.add_device("125", "binarysensor")
+    def setup_app(self):
+        # specify our message handler method
+        self.connection.add_handler(self.message_handler)
 
-# then we add a BACKGROUND thread. This is not required and
-# just shows how to send events from a separate thread. This might
-# be handy when you have to poll something in the BACKGROUND or need
-# to handle some other communication. If you don't need one or if
-# you want to keep things simple at the moment just skip this section.
+        # If we want to use the custom command line argument, we look at
+        # self.args:
+        if self.args.test:
+            self.log.info("Test argument was set")
+        else:
+            self.log.warning("Test argument was NOT set")
+
+        # if you need to fetch any settings from config.ini,
+        # use the self.get_config_option method.
+        # This will look in the configuration file conf.d/<app name>.conf
+        # under the [<app name>] section.
+        # In this example, this means example.conf and [example] section
+        param = self.get_config_option("parameter", "0")
+        self.log.info("Configuration parameter was set to %s", param)
+
+        # Now you need to tell the AgoConnection object about the
+        # devices you provide.
+        #
+        # The add_device call expects a internal id
+        # and a device type (you can find all valid types in the
+        # schema.yaml configuration file). The internal id is whatever
+        # you're using in your code to distinct your devices.
+        # Or the pin number of some GPIO output. Or the IP of a
+        # networked device. Whatever fits your device specific stuff.
+        # The persistent translation to a ago control uuid will be
+        # done by the AgoConnection object, by storing the mappings as a json
+        # file in CONFDIR/uuidmap/<instance name>.json.
+        # You don't need to worry at all about this, when the
+        # message_handler is called, you'll be passed the internalid
+        # for the device that you did specifiy when using add_device()
+
+        # we add a switch and a dimmer
+        self.connection.add_device("123", "dimmer")
+        self.connection.add_device("124", "switch")
+
+        # for our threading example in the next section we also add a binary sensor:
+        self.connection.add_device("125", "binarysensor")
+
+        # then we add a BACKGROUND thread. This is not required and
+        # just shows how to send events from a separate thread. This might
+        # be handy when you have to poll something in the BACKGROUND or need
+        # to handle some other communication. If you don't need one or if
+        # you want to keep things simple at the moment just skip this section.
+        BACKGROUND = TestEvent(self)
+        BACKGROUND.setDaemon(True)
+        BACKGROUND.start()
+
+        # now you should have added all devices, set up all your internal
+        # and device specific stuff, started everything like listener threads
+        # or whatever.
+        # setup_app will now return, and AgoApp will do the rest.
+        # This normally means calling app_main, which calls the run() method
+        # on the AgoConnection object, which will block and start message handling.
+
+    def app_cleanup(self):
+        # When our app is about to shutdown, we should clean up any resources we've
+        # allocated in app_setup. This is done here.
+        # In this example, we do not have any resources..
+        pass
+
 
 
 class TestEvent(threading.Thread):
     """Test Event."""
-    def __init__(self,):
+    def __init__(self, app):
         threading.Thread.__init__(self)
+        self.app = app
 
     def run(self):
         level = 0
-        while (True):
-            CLIENT.emit_event("125",
+        # This is a dummy backend thread. It loops forever.
+        # It is important that we exit when told to, by checking is_exit_signaled.
+        #
+        # TODO: Improve this example; we do not handle proper shutdown..
+        while not self.app.is_exit_signaled():
+            self.app.connection.emit_event("125",
                 "event.security.sensortriggered", level, "")
             if (level == 0):
                 level = 255
@@ -118,13 +167,8 @@ class TestEvent(threading.Thread):
                 level = 0
             time.sleep(5)
 
-BACKGROUND = TestEvent()
-BACKGROUND.setDaemon(True)
-BACKGROUND.start()
+# Finally, but very important.
+# We must call the main function of the application object
+if __name__ == "__main__":
+    AgoExample().main()
 
-
-# now you should have added all devices, set up all your internal
-# and device specific stuff, started everything like listener threads
-# or whatever. The call to run() is blocking and
-# will start the message handling
-CLIENT.run()
