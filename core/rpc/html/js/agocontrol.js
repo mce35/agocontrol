mce35/agocontrol

commit 7f5a1cf4fe37b9c16901ad992164cccce0382c40

Display dashboard (old floorplan) name as user wrote it

core/rpc/html/js/agocontrol.js
@@ -178,7 +178,7 @@ Agocontrol.prototype = {
 
         //fill members
         self.inventory = response.result;
-        self.dashboards.push({name:'all', ucName:'All my devices', action:'', editable:false});
+        self.dashboards.push({name:'All my devices', ucName:'All my devices', action:'', editable:false});
         for( uuid in response.result.floorplans )
         {
             var dashboard = response.result.floorplans[uuid];

core/rpc/html/index.html
@@ -89,7 +89,7 @@
                     </a>
                     <div class="dropdown">
                         <ul data-bind="foreach: agocontrol.dashboards">
-                            <li><a href="#" data-bind="text:ucName, click:$root.gotoDashboard"></a></li>
+                            <li><a href="#" data-bind="text:name, click:$root.gotoDashboard"></a></li>
                         </ul>
                     </div>
                 </li>

commit 321c4b6bd19811a79017ec3e871d74b3e8c496f6

Fix bug introduced in previous commit 7f5a1cf4fe37b9c16901ad992164cccce0382c40

core/rpc/html/js/agocontrol.js
@@ -178,13 +178,13 @@ Agocontrol.prototype = {
 
         //fill members
         self.inventory = response.result;
-        self.dashboards.push({name:'All my devices', ucName:'All my devices', action:'', editable:false});
+        self.dashboards.push({name:'all', ucName:'All my devices', action:'', editable:false});
         for( uuid in response.result.floorplans )
         {
             var dashboard = response.result.floorplans[uuid];
             dashboard.uuid = uuid;
             dashboard.action = '';
-            dashboard.ucName = ucFirst(dashboard.name);
+            dashboard.ucName = dashboard.name;
             dashboard.editable = true;
             self.dashboards.push(dashboard);
         }