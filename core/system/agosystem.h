aafa891d7382bb270a794485b1a53df69c78ce57
Johan Ström <johan@stromnet.se>
2015-03-01 22:33:40 +0100

    FreeBSD fixes for agosystem, move platform specific code to separate files
    Note that there is no standard bin-path in FreeBSD yet so file detection does not work.

---
+++ core/system/agosystem.h
@@ -0,0 +1,32 @@
+#ifndef AGOSYSTEM_H
+#define AGOSYSTEM_H
+#include "agoapp.h"
+
+using namespace agocontrol;
+
+class AgoSystem: public AgoApp {
+private:
+    qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) ;
+
+    qpid::types::Variant::Map getAgoProcessList();
+    qpid::types::Variant::Map getAgoProcessListDebug(std::string procName);
+
+    // Implemented in processinfo_<platform>.cpp
+    void getProcessInfo();
+
+    void fillProcessesStats(qpid::types::Variant::Map& processes);
+    void checkProcessesStates(qpid::types::Variant::Map& processes);
+    void printProcess(qpid::types::Variant::Map process);
+    void monitorProcesses();
+    qpid::types::Variant::Map getProcessStructure();
+
+    void setupApp();
+
+    boost::mutex processesMutex;
+    qpid::types::Variant::Map processes;
+    qpid::types::Variant::Map config;
+public:
+    AGOAPP_CONSTRUCTOR(AgoSystem);
+};
+
+#endif
