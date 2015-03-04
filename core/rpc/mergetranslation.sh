80222378143631a93045c5c133f8a66a7c401f93
Johan Ström <johan@stromnet.se>
2015-03-04 20:47:34 +0100

    Ensure mergetranslations fails build process if intltool fails

--- core/rpc/mergetranslation.sh
+++ core/rpc/mergetranslation.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 TMPFILE=$(mktemp -u).xml.in
 SOURCEFILE=$(realpath $1)
-ln -s ${SOURCEFILE} ${TMPFILE}
-intltool-merge -x html/po ${TMPFILE} $2
+ln -s ${SOURCEFILE} ${TMPFILE} || exit 1
+intltool-merge -x html/po ${TMPFILE} $2 || exit 1
 
 # Remove first line: <?xml version...
 # sed -i is not POSIX, and behaves differently on BSD vs GNU
 if [ `uname -s` == "FreeBSD" ]; then
-	sed -i '' '1d' $2
+	sed -i '' '1d' $2 || exit 1
 else
-	sed -i '1d' $2
+	sed -i '1d' $2 || exit 1
 fi
 
 rm ${TMPFILE}
