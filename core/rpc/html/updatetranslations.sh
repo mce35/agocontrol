97f04d87fcb7b33c86da4988a5ba9845da4025d6
Tang <tanguy.bonneau@gmail.com>
2015-03-11 11:40:44 +0100

    Restore updatetranslations.sh file and update it

---
+++ core/rpc/html/updatetranslations.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+cd templates
+for i in `find . -name \*in.html`;
+    do ln -s `basename "$i"` "${i%.*.*}.xml.in";
+done
+
+cd ..
+cd applications
+for i in `find . -name \*in.html`;
+    do ln -s `basename "$i"` "${i%.*.*}.xml.in";
+done
+
+cd ..
+cd configuration
+for i in `find . -name \*in.html`;
+    do ln -s `basename "$i"` "${i%.*.*}.xml.in";
+done
+
+cd ..
+cd help
+for i in `find . -name \*in.html`;
+    do ln -s `basename "$i"` "${i%.*.*}.xml.in";
+done
+
+cd ..
+cd po
+intltool-update -p -x -g agocontrol
+for i in *.po;
+    do msgmerge -U "$i" agocontrol.pot;
+done
+
+cd ..
+for i in `find . -name \*xml.in`;
+    do rm "$i";
+done
+
+
