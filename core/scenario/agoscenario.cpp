4ee91cc685185a83e08ca96691e6ded414803bfb
Tang <tanguy.bonneau@gmail.com>
2015-03-30 14:54:22 +0200

    Update scenario to new response format

--- core/scenario/agoscenario.cpp
+++ core/scenario/agoscenario.cpp
@@ -16,7 +16,8 @@
 using namespace std;
 using namespace agocontrol;
 
-class AgoScenario: public AgoApp {
+class AgoScenario: public AgoApp
+{
 private:
     qpid::types::Variant::Map scenariomap;
     qpid::types::Variant::Map commandHandler(qpid::types::Variant::Map content) ;
@@ -27,112 +28,152 @@ public:
     AGOAPP_CONSTRUCTOR(AgoScenario);
 };
 
-void AgoScenario::runscenario(qpid::types::Variant::Map &scenario) {
+void AgoScenario::runscenario(qpid::types::Variant::Map &scenario)
+{
     AGO_DEBUG() << "Executing scenario";
 
     // build sorted list of scenario elements
     std::list<int> elements;
-    for (qpid::types::Variant::Map::const_iterator it = scenario.begin(); it!= scenario.end(); it++) {
+    for (qpid::types::Variant::Map::const_iterator it = scenario.begin(); it!= scenario.end(); it++)
+    {
         // AGO_TRACE() << it->first;
         // AGO_TRACE() << it->second;
         elements.push_back(atoi(it->first.c_str()));
     }
     // AGO_TRACE() << "elements: " << elements;
     elements.sort();
-    for (std::list<int>::const_iterator it = elements.begin(); it != elements.end(); it++) {
+    for (std::list<int>::const_iterator it = elements.begin(); it != elements.end(); it++)
+    {
         // AGO_TRACE() << *it;
         int seq = *it;
         stringstream sseq;
         sseq << seq;
         qpid::types::Variant::Map element = scenario[sseq.str()].asMap();
         AGO_TRACE() << sseq.str() << ": " << scenario[sseq.str()];
-        if (element["command"] == "scenariosleep") {
-            try {
+        if (element["command"] == "scenariosleep")
+        {
+            try
+            {
                 int delay = element["delay"];
                 AGO_DEBUG() << "scenariosleep special command detected. Delay: " << delay;
                 sleep(delay);
-            } catch (qpid::types::InvalidConversion) {
+            }
+            catch (qpid::types::InvalidConversion)
+            {
                 AGO_ERROR() << "Invalid conversion of delay value";
             }
-        } else {
+        }
+        else
+        {
             AGO_DEBUG() << "sending scenario command: " << element;
             agoConnection->sendMessage(element);
         }
     }
 }
 
-qpid::types::Variant::Map AgoScenario::commandHandler(qpid::types::Variant::Map content) {
-    qpid::types::Variant::Map returnval;
+qpid::types::Variant::Map AgoScenario::commandHandler(qpid::types::Variant::Map content)
+{
+    qpid::types::Variant::Map returnData;
     std::string internalid = content["internalid"].asString();
-    if (internalid == "scenariocontroller") {
-        if (content["command"] == "setscenario") {
-            try {
+
+    if (internalid == "scenariocontroller")
+    {
+        if (content["command"] == "setscenario")
+        {
+            try
+            {
                 AGO_TRACE() << "setscenario request";
                 qpid::types::Variant::Map newscenario = content["scenariomap"].asMap();
                 AGO_TRACE() << "scnario content:" << newscenario;
                 std::string scenariouuid = content["scenario"].asString();
-                if (scenariouuid == "") scenariouuid = generateUuid();
+                if (scenariouuid == "")
+                {
+                    scenariouuid = generateUuid();
+                }
                 AGO_TRACE() << "scenario uuid:" << scenariouuid;
                 scenariomap[scenariouuid] = newscenario;
                 agoConnection->addDevice(scenariouuid.c_str(), "scenario", true);
-                if (variantMapToJSONFile(scenariomap, getConfigPath(SCENARIOMAPFILE))) {
-                    returnval["result"] = 0;
-                    returnval["scenario"] = scenariouuid;
-                } else {
-                    returnval["result"] = -1;
+                if (variantMapToJSONFile(scenariomap, getConfigPath(SCENARIOMAPFILE)))
+                {
+                    returnData["scenario"] = scenariouuid;
+                    return responseSuccess(returnData);
                 }
-            } catch (qpid::types::InvalidConversion) {
-                returnval["result"] = -1;
-            } catch (...) {
-                returnval["result"] = -1;
-                returnval["error"] = "exception";
+                else
+                {
+                    return responseFailed();
+                }
             }
-        } else if (content["command"] == "getscenario") {
-            try {
+            catch (qpid::types::InvalidConversion)
+            {
+                return responseFailed();
+            }
+            catch (...)
+            {
+                return responseFailed("Exception");
+            }
+        }
+        else if (content["command"] == "getscenario")
+        {
+            try
+            {
                 std::string scenario = content["scenario"].asString();
                 AGO_TRACE() << "getscenario request:" << scenario;
-                returnval["result"] = 0;
-                returnval["scenariomap"] = scenariomap[scenario].asMap();
-                returnval["scenario"] = scenario;
-            } catch (qpid::types::InvalidConversion) {
-                returnval["result"] = -1;
+                returnData["scenariomap"] = scenariomap[scenario].asMap();
+                returnData["scenario"] = scenario;
+                return responseSuccess(returnData);
             }
-        } else if (content["command"] == "delscenario") {
+            catch (qpid::types::InvalidConversion)
+            {
+                return responseFailed();
+            }
+        }
+        else if (content["command"] == "delscenario")
+        {
             std::string scenario = content["scenario"].asString();
             AGO_TRACE() << "delscenario request:" << scenario;
-            returnval["result"] = -1;
-            if (scenario != "") {
+            if (scenario != "")
+            {
                 qpid::types::Variant::Map::iterator it = scenariomap.find(scenario);
-                if (it != scenariomap.end()) {
+                if (it != scenariomap.end())
+                {
                     AGO_DEBUG() << "removing ago device" << scenario;
                     agoConnection->removeDevice(it->first.c_str());
                     scenariomap.erase(it);
-                    if (variantMapToJSONFile(scenariomap, getConfigPath(SCENARIOMAPFILE))) {
-                        returnval["result"] = 0;
+                    if (variantMapToJSONFile(scenariomap, getConfigPath(SCENARIOMAPFILE)))
+                    {
+                        return responseSuccess();
                     }
                 }
             }
+            else
+            {
+                return responseFailed();
+            }
         } 
-
-    } else {
-
-        if ((content["command"] == "on") || (content["command"] == "run")) {
+    }
+    else
+    {
+        if ((content["command"] == "on") || (content["command"] == "run"))
+        {
             AGO_DEBUG() << "spawning thread for scenario: " << internalid;
             boost::thread t(boost::bind(&AgoScenario::runscenario, this, scenariomap[internalid].asMap()));
             t.detach();
-            returnval["result"] = 0;
+            return responseSuccess();
         } 
-
     }
-    return returnval;
+
+    //We do not support sending commands to our 'devices'
+    return responseError(RESPONSE_ERR_NO_COMMANDS_FOR_DEVICE);
 }
 
-void AgoScenario::setupApp() {
+void AgoScenario::setupApp()
+{
     addCommandHandler();
     agoConnection->addDevice("scenariocontroller", "scenariocontroller");
 
     scenariomap = jsonFileToVariantMap(getConfigPath(SCENARIOMAPFILE));
-    for (qpid::types::Variant::Map::const_iterator it = scenariomap.begin(); it!=scenariomap.end(); it++) {
+    for (qpid::types::Variant::Map::const_iterator it = scenariomap.begin(); it!=scenariomap.end(); it++)
+    {
         AGO_DEBUG() << "Loading scenario: " << it->first << ":" << it->second;
         agoConnection->addDevice(it->first.c_str(), "scenario", true);
     }
