ccdc53be8fb376ab27ce08f09493c70276c1d9df
Tang <tanguy.bonneau+ago@gmail.com>
2015-07-03 10:17:07 +0200

    Update debug page

--- core/rpc/html/help/debug/template.js
+++ core/rpc/html/help/debug/template.js
@@ -23,11 +23,11 @@ function Debug(agocontrol)
             //append new message
             if( type===undefined )
             {
-                $('#drainContainer > ul').append('<li style="font-size:small;" class="default alert">'+d+' : '+JSON.stringify(event)+'</i>');
+                $('#drainContainer > ul').append('<li style="font-size:small;" class="list-group-item">'+d+' : '+JSON.stringify(event)+'</i>');
             }
             else if( type=='start' || type=='stop' )
             {
-                $('#drainContainer > ul').append('<li style="font-size:small;" class="primary alert">'+d+' : '+JSON.stringify(event)+'</i>');
+                $('#drainContainer > ul').append('<li style="font-size:small;" class="list-group-item" list-group-item-info>'+d+' : '+JSON.stringify(event)+'</i>');
             }
 
             //scroll down
@@ -65,79 +65,6 @@ function Debug(agocontrol)
     self.clear = function()
     {
         $('#drainContainer > ul').empty();
-    };
-
-    //==============
-    //CONFIGURATION
-    //==============
-    self.configTree = ko.observableArray([]);
-    self.configTreeGrid = new ko.agoGrid.viewModel({
-        data: self.configTree,
-        columns: [
-            {headerText:'Parameter', rowText:'name'},
-            {headerText:'Value', rowText:''},
-            {headerText:'Action', rowText:''}
-        ],
-        rowTemplate: 'configTreeRowTemplate',
-        pageSize: 100
-    });
-
-    //get config tree
-    self.getConfigTree = function()
-    {
-        var content = {};
-        content.command = 'getconfigtree';
-        content.uuid = self.agocontrol.agoController;
-        self.agocontrol.sendCommand(content)
-            .then(function(res) {
-                if( res.data.config )
-                {
-                    var configs = [];
-                    for( var app in res.data.config )
-                    {
-                        for( var section in res.data.config[app] )
-                        {
-                            for( var option in res.data.config[app][section] )
-                            {
-                                //filter comments
-                                if( option.indexOf('#')!=0 )
-                                {
-                                    configs.push({
-                                        name : app+'/'+section+'/'+option,
-                                        app: app,
-                                        section: section,
-                                        option: option,
-                                        oldValue: res.data.config[app][section][option],
-                                        value: ko.observable(res.data.config[app][section][option])
-                                    });
-                                }
-                            }
-                        }
-                    }
-                    self.configTree(configs);
-                }
-            });
-    };
-    self.getConfigTree();
-
-    //save specified config param
-    self.setConfig = function(param)
-    {
-        var content = {};
-        content.uuid = self.agocontrol.agoController;
-        content.command = 'setconfig';
-        content.app = param.app;
-        content.section = param.section;
-        content.option = param.option;
-        content.value = param.value(); //observable
-        self.agocontrol.sendCommand(content)
-            .then(function(res) {
-                //success
-                notif.success('Parameter value updated');
-            })
-            .catch(function(err) {
-                param.value(param.oldValue);
-            });
     };
 };
 
