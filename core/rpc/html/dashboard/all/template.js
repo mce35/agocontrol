2391bd9f3fc9824848929de96904f2d9bd96297c
Tang <tanguy.bonneau+ago@gmail.com>
2015-07-13 16:09:27 +0200

    Update main dashboard and remove deprecated code

--- core/rpc/html/dashboard/all/template.js
+++ core/rpc/html/dashboard/all/template.js
@@ -7,7 +7,6 @@ function dashBoard(agocontrol)
     var self = this;
     self.agocontrol = agocontrol;
     self.itemsPerPage = 12;
-    self.itemsPerRow = 4;
     self.keyword = ko.observable("");
     self.currentPage = ko.observable(1);
     self.deviceList = ko.computed(function()
@@ -46,7 +45,6 @@ function dashBoard(agocontrol)
 
     self.devicesPerPage = ko.computed(function()
     {
-        //return self.deviceList().chunk(self.itemsPerPage);
         var currentList = self.deviceList().chunk(self.itemsPerPage);
         if (currentList.length < self.currentPage())
         {
@@ -65,51 +63,6 @@ function dashBoard(agocontrol)
             pages.push({idx : i});
         }
         return pages;
-    });
-
-    self.firstRow = ko.computed(function()
-    {
-        var currentList = self.deviceList().chunk(self.itemsPerPage);
-        if (currentList.length < self.currentPage())
-        {
-            return [];
-        }
-        currentList = currentList[self.currentPage() - 1];
-        if (currentList.length >= 0)
-        {
-            return currentList.chunk(self.itemsPerRow)[0];
-        }
-        return [];
-    });
-
-    self.secondRow = ko.computed(function()
-    {
-        var currentList = self.deviceList().chunk(self.itemsPerPage);
-        if (currentList.length < self.currentPage())
-        {
-            return [];
-        }
-        currentList = currentList[self.currentPage() - 1];
-        if (currentList.length >= (self.itemsPerRow+1))
-        {
-            return currentList.chunk(self.itemsPerRow)[1];
-        }
-        return [];
-    });
-
-    self.thirdRow = ko.computed(function()
-    {
-        var currentList = self.deviceList().chunk(self.itemsPerPage);
-        if (currentList.length < self.currentPage())
-        {
-            return [];
-        }
-        currentList = currentList[self.currentPage() - 1];
-        if (currentList.length >= (self.itemsPerRow*2+1))
-        {
-            return currentList.chunk(self.itemsPerRow)[2];
-        }
-        return [];
     });
 
     //change page
