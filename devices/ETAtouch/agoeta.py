6a976a9d4e41347e18d488d763a637a1ccb2bb05
Christoph Jäger <jaeger@agocontrol.com>
2015-09-16 12:13:20 +0200

    updated release of ETA Touch heater control. Created a app and JSON config file for devices.

--- devices/ETAtouch/agoeta.py
+++ devices/ETAtouch/agoeta.py
@@ -1,93 +1,106 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 # ETAtouch REST API support for ago control
 # More information about ETAtouch: http://www.eta.co.at/255.0.html
 #
-# copyright (c) 2014 Christoph Jaeger <jaeger@agocontrol.com>
+# copyright (c) 2015 Christoph Jaeger <jaeger@agocontrol.com>
 #
 
 import agoclient
 import threading
 import time
+import os
+import json
 import urllib2
 import xml.etree.ElementTree as ET
 
-client = agoclient.AgoConnection("eta")
-
-eta_ip = agoclient.get_config_option("eta", "IP", "127.0.0.1")
-
-base_url = 'http://' + eta_ip + ':8080/user/var/'
 eta_result = {}
 eta_result_report = {}
-eta_url = {'temperatur_aussen': '48/10241/0/0/12197', 'temperatur_raum': '120/10101/0/0/12634', 'puffer_ladezustand': '120/10251/0/0/12528', 'puffer_oben': '120/10251/0/0/12242', 'puffer_mitte': '120/10251/0/0/12522', 'puffer_unten': '120/10251/0/0/12244', 'warmwasserspeicher_temperatur': '120/10111/0/0/12271', 'abgas_restsauerstoff': '48/10391/0/0/12164', 'abgas_temperatur': '48/10391/0/0/12162' }
+devices = {}
 
-# add the devices
-for key in eta_url:
-  client.add_device(key, "multilevelsensor")
+class AgoEta(agoclient.AgoApp):
+    def setup_app(self):
+        # read IP from config file
+        eta_ip = self.get_config_option("IP", "127.0.0.1")
+        self.log.info("ETA IP-Address is: %s", eta_ip)
 
-# emit result value and do checks 
-def emit_result(key):
-    if key == "puffer_ladezustand":
-        client.emit_event(key, "event.environment.temperaturechanged", eta_result_report[key], "percent")
-    elif key == "abgas_restsauerstoff":
-        client.emit_event(key, "event.environment.temperaturechanged", eta_result_report[key], "percent")
+        # define Base URL
+        global base_url
+        base_url = 'http://' + eta_ip + ':8080/user/var/'
+
+        # JSON Device Config 
+        devices_config_file = agoclient.config.get_config_path('maps/eta.json')
+        if os.path.isfile(devices_config_file) and os.access(devices_config_file, os.R_OK):
+            self.log.debug("JSON config file %s exists" % devices_config_file)
+            with open(devices_config_file) as devices_config_file_json:
+                devices_config = json.load(devices_config_file_json)
+                global devices
+                devices = devices_config['devices']
+                for device in devices:
+                    value = devices[device]
+                    self.log.debug("ID=({}) URL=({}) DEVICETYPE=({})".format(device, devices[device]["url"], devices[device]["devicetype"]))
+                    self.connection.add_device(device, devices[device]["devicetype"])
+        else:
+            self.log.warning("JSON config file %s NOT exist" % DevicesConfigFile)
+
+        BACKGROUND = AgoEtaEvent(self)
+        BACKGROUND.setDaemon(True)
+        BACKGROUND.start()
+
+    def app_cleanup(self):
+        pass
+
+
+def emit_result(self,key):
+    if devices[key]["devicetype"] == "multilevelsensor":
+        self.app.connection.emit_event(key, "event.environment.counterchanged", eta_result_report[key], "percent")
     else:
-        client.emit_event(key, "event.environment.temperaturechanged", eta_result_report[key], "degC")
+        self.app.connection.emit_event(key, "event.environment.temperaturechanged", eta_result_report[key], "degC")
 
-# check the two dicts if key exists and if value matches
-def get_diffs(a,b):
-    for a_key in a:
-        # check if key from a exists in b
-        if a_key not in b:
-            #print "key %s in a, but not in b" %a_key
-            eta_result_report[a_key] = eta_result[a_key]
-            # report result
-            emit_result(a_key)
+class AgoEtaEvent(threading.Thread):
+    def __init__(self, app):
+        threading.Thread.__init__(self)
+        self.app = app
 
-        # check if key from a has same value in key of b
-        elif a[a_key] != b[a_key]:
-            #print "key %s in a and in b, but values differ (%s in a and %s in b)" %(a_key, a[a_key], b[a_key])
-            eta_result_report[a_key] = eta_result[a_key]
-            # report result
-            emit_result(a_key)
-	
-
-class eta_event(threading.Thread):
-    def __init__(self,):
-        threading.Thread.__init__(self)    
     def run(self):
-    	level = 0
-        while (True):
-			try:
-			    for key in eta_url:
-			        request = urllib2.urlopen(base_url + eta_url[key])
-			        tree = ET.parse(request)
-			        root = tree.getroot()
-			        result = root.find("{http://www.eta.co.at/rest/v1}value")
+        level = 0
+        while not self.app.is_exit_signaled():
+            try:
+                for device in devices:
+                    request = urllib2.urlopen(base_url + devices[device]["url"])
+                    tree = ET.parse(request)
+                    root = tree.getroot()
+                    result = root.find("{http://www.eta.co.at/rest/v1}value")
+                    
+                    if result.attrib['strValue'] == "0":
+                        eta_result[device] = "0"
+                    elif result.attrib['strValue'] == "---":
+                        eta_result[device] = "0"
+                    else:
+                        try:
+                            eta_result[device] = float(result.attrib['strValue'].replace(',','.'))
+                        except:
+                            eta_result[device] = result.attrib['strValue']
 
-			        if result.attrib['strValue'] == "0":
-			            eta_result[key] = "0"
-			        elif result.attrib['strValue'] == "---":
-			            eta_result[key] = "0"
-			        else:
-			            try:
-			                eta_result[key] = float(result.attrib['strValue'].replace(',','.'))
-			            except:
-			                eta_result[key] = result.attrib['strValue']
+                    # check when we get a new value and announce it
+                    for a_key in eta_result:
+                        # check if key from a exists in b
+                        if a_key not in eta_result_report:
+                            eta_result_report[a_key] = eta_result[a_key]
+                            emit_result(self, a_key)
 
-			        # compare results and emit on change
-			        get_diffs(eta_result,eta_result_report)
+                        elif eta_result[a_key] != eta_result_report[a_key]:
+                            eta_result_report[a_key] = eta_result[a_key]
+                            emit_result(self, a_key)
 
-			except urllib2.HTTPError, e:
-				print 'We failed with error code - %s.' % e.code
 
-			except urllib2.URLError, e:
-				print 'We failed with error - %s.' % e.reason
+            except urllib2.HTTPError, e:
+                print 'We failed with error code - %s.' % e.code
 
-			time.sleep (60)
-      
-background = eta_event()
-background.setDaemon(True)
-background.start()
+            except urllib2.URLError, e:
+                print 'We failed with error - %s.' % e.reason
 
-client.run()
+            time.sleep(10)
+
+if __name__ == "__main__":
+    AgoEta().main()
 
