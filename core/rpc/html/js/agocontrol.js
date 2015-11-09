b452418dac41de2389e11c6cc6d724ab018ce556
Tang <tanguy.bonneau+ago@gmail.com>
2015-11-09 17:05:44 +0100

    Update bluebird to latest version (2.9.19->3.0.5). Fix bug in resources loading

--- core/rpc/html/js/agocontrol.js
+++ core/rpc/html/js/agocontrol.js
@@ -14,8 +14,8 @@ Agocontrol.prototype = {
     eventHandlers: [],
     _allApplications: ko.observableArray([]),
     _allProtocols: ko.observableArray([]),
-    _getApplications: Promise.pending(),
-    _getProtocols: Promise.pending(),
+    _getApplications: new Promise(function(){}),
+    _getProtocols: new Promise(function(){}),
     _favorites: ko.observable(),
     _noProcesses: ko.observable(false),
 
@@ -118,7 +118,7 @@ Agocontrol.prototype = {
             }
 
             this.applications(applications);
-            this._getApplications.fulfill();
+            this._getApplications.then();
 
             //protocols
             var protocols = [];
@@ -148,7 +148,7 @@ Agocontrol.prototype = {
             }
 
             this.protocols(protocols);
-            this._getProtocols.fulfill();
+            this._getProtocols.then();
 
         }, this);
 
