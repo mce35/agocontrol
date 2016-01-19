7e4bce266bfce5382b15697f470fb33d6bb7cff1
Andreas Pagander <andreas.pagander@gmail.com>
2016-01-19 15:54:52 +0100

    Update raspiCamera to use response_success and workaround for qpid 65k limit

--- devices/raspiCamera/raspiCamera.py
+++ devices/raspiCamera/raspiCamera.py
@@ -17,6 +17,7 @@ import time
 
 client = agoclient.AgoConnection("raspiCamera")
 
+
 def messageHandler(internalid, content):
     result = {}
     result["result"] = -1;
@@ -25,19 +26,16 @@ def messageHandler(internalid, content):
             print "raspiCamera getting video frame"
             stream = io.BytesIO()
             with picamera.PiCamera() as camera:
-                camera.resolution = (200, 140)
-                camera.quality = (20)
+                camera.resolution = (1024, 768)
+                camera.quality = (90)
                 camera.start_preview()
                 time.sleep(0.1)
                 camera.capture(stream, format='jpeg')
-            result["image"] = base64.b64encode(stream.getvalue())
-            result["result"] = 0;
-            print "result[""image""] sending"
+                frame = stream.getvalue()
 
-    return result
+    return client.response_success({'image':buffer(base64.b64encode(frame))})
 
 client.add_handler(messageHandler)
-#devicelist=agoclient.get_config_option("raspiCamera", "devices", "")
 
 client.add_device("raspiCamera", "camera")
 
