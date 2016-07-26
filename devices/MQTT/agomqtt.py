f8991820e363b5acb3fca887d3976cbc9ac9bc19
Tempura <tempura.san@gmail.com>
2016-07-26 20:52:07 +0200

    agomqtt: removed devicetype from MQTT device storage

--- devices/MQTT/agomqtt.py
+++ devices/MQTT/agomqtt.py
@@ -20,22 +20,46 @@ import time
 
 class AgoMQTT(agoclient.AgoApp):
     """ago control MQTT device"""
-
+    
     def setup_app(self):
         self.mqtt_broker = self.get_config_option("broker", "127.0.0.1")
         self.mqtt_port = self.get_config_option("port", "1883")
         self.topic = self.get_config_option("topic", "sensors/#")
         self.mapping = self.get_config_option("mapping")
 
+        self.map = {
+            "temperature":["temperaturesensor", "event.environment.temperaturechanged", "degC"],
+            "humidity":["humiditysensor", "event.environment.humiditychanged", "%"],
+            "pressure":["barometersensor", "event.environment.pressurechanged", "mBar"]
+        }
+
+        if self.mapping:
+            for mapping in self.mapping.split(";"):
+                parts = mapping.split(":")
+                if len(parts) != 4:
+                    self.log.error("Wrong number of parameters for mapping - skipping entry '%s'", mapping)
+                    continue
+                
+                # do not permit to overwrite existing mappings (for backwards compatibility)
+                if not parts[0] in self.map:
+                    self.log.info("Adding new mapping for: %s (%s)", parts[0], ",".join(parts[1:]))
+                    self.map[parts[0]] = parts[1:]
+                else:
+                    self.log.warning("Skipping existing mapping for: %s", parts[0])
+
         # load existing device list, add and mark devices as stale
         self.log.info("Loading MQTT device list...")
         try:
             with open(agoclient.config.get_localstate_path() + "/mqtt_devices.json") as store:
                 devicelist = json.load(store)
-            for internalid in devicelist.keys():
-                self.log.trace("Restoring MQTT device %s", internalid)
-                self.connection.add_device(internalid, devicelist[internalid])
-                self.connection.suspend_device(internalid)
+            for internalid in devicelist:
+                (devicetype, _, _) = self.getMappingForTopic(internalid)
+                if devicetype is not None:
+                    self.log.trace("Restoring MQTT device %s as %s", internalid, devicetype)
+                    self.connection.add_device(internalid, devicetype)
+                    self.connection.suspend_device(internalid)
+                else:
+                    self.log.warning("Will not restore MQTT device %s - no known mapping exists", internalid)
         except (OSError, IOError) as exception:
             if exception.errno == errno.ENOENT:
                 self.log.debug("MQTT device list not found - starting fresh")
@@ -52,11 +76,11 @@ class AgoMQTT(agoclient.AgoApp):
         try:
             inventory = self.connection.get_inventory()
             if inventory:
-                devicelist = {}
+                devicelist = []
                 for _, d in inventory["devices"].items():
                     if d["handled-by"] == "mqtt":
                         self.log.trace("Storing MQTT device %s", d["internalid"])
-                        devicelist[d["internalid"]] = d["devicetype"]
+                        devicelist.append(d["internalid"])
                 with open(agoclient.config.get_localstate_path() + "/mqtt_devices.json", "w") as store:
                     json.dump(devicelist, store)
             else:
@@ -67,34 +91,18 @@ class AgoMQTT(agoclient.AgoApp):
             self.log.error("Failed to encode MQTT device list - reason: %s", exception)
         self.worker.join()
 
+    def getMappingForTopic(self, topic):
+        for key in self.map.keys():
+            if topic.find(key) != -1:
+                return self.map[key]
+        return (None, None, None)
+
 class MQTTThread(threading.Thread):
     """MQTTThread"""
     def __init__(self, app):
         threading.Thread.__init__(self)
         self.app = app
         self.connected = False
-        self.mapping = {
-            "temperature":["temperaturesensor", "event.environment.temperaturechanged", "degC"],
-            "humidity":["humiditysensor", "event.environment.humiditychanged", "%"],
-            "pressure":["barometersensor", "event.environment.pressurechanged", "mBar"],
-            "power":["powermeter", "event.environment.powerchanged", "W"],
-            "flow":["flowmeter", "event.environment.flowchanged", "m^3"],
-            "energy":["energymeter", "event.environment.energychanged", "kWh"]
-        }
-        
-        if self.app.mapping:
-            for mapping in self.app.mapping.split(";"):
-                parts = mapping.split(":")
-                if len(parts) != 4:
-                    self.app.log.error("Wrong number of parameters for mapping - skipping entry '%s'", mapping)
-                    continue
-                
-                # do not permit to overwrite existing mappings (for backwards compatibility)
-                if not parts[0] in self.mapping:
-                    self.app.log.info("Adding new mapping for: %s (%s)", parts[0], ",".join(parts[1:]))
-                    self.mapping[parts[0]] = parts[1:]
-                else:
-                    self.app.log.warning("Skipping existing mapping for: %s", parts[0])
 
     def on_connect(self, client, obj, flags, rc):
         self.app.log.info("Connected to MQTT broker %s:%s (rc=%d, %s)", self.app.mqtt_broker, self.app.mqtt_port, rc, mqtt.error_string(rc))
@@ -106,16 +114,14 @@ class MQTTThread(threading.Thread):
 
     def on_message(self, client, obj, msg):
         self.app.log.info("Received MQTT message on topic %s: %s", msg.topic, msg.payload)
-        for key in self.mapping.keys():
-            if msg.topic.find(key) != -1:
-                (devicetype, event, unit) = self.mapping[key]
-                self.app.log.debug("Matched key '%s' for topic '%s' - emitting '%s'", key, msg.topic, event)
-                # TODO is it necessary to check here to avoid multiple additions?
-                if self.app.connection.internal_id_to_uuid(msg.topic) is None:
-                    self.app.connection.add_device(msg.topic, devicetype)
-                self.app.connection.resume_device(msg.topic)
-                self.app.connection.emit_event(msg.topic, event, float(msg.payload), unit)
-                break
+        (devicetype, event, unit) = self.app.getMappingForTopic(msg.topic)
+        if devicetype is not None:
+            self.app.log.debug("Matched devicetype '%s' for topic '%s' - emitting '%s'", devicetype, msg.topic, event)
+            # TODO is it necessary to check here to avoid multiple additions?
+            if self.app.connection.internal_id_to_uuid(msg.topic) is None:
+                self.app.connection.add_device(msg.topic, devicetype)
+            self.app.connection.resume_device(msg.topic)
+            self.app.connection.emit_event(msg.topic, event, float(msg.payload), unit)
 
     def on_log(self, client, obj, level, string):
         self.app.log.debug("Paho log: %s %s", str(level), string)
