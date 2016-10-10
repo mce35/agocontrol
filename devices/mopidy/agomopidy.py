d81306b1fdab9a04a1fa1f77ff1cdabc2036ef44
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2016-10-10 22:54:07 +0200

    Mopidy: Websocket listning to media events added + thread to collet the into and emit to web UI

--- devices/mopidy/agomopidy.py
+++ devices/mopidy/agomopidy.py
@@ -16,9 +16,19 @@ __version__ = AGO_MOPIDY_VERSION
 ############################################
 
 import agoclient
-import threading
+import threading, Queue
 import time
+import websocket
 from mopidy import Mopidy
+from mopidy import RunningStates
+from mopidy import PlayingStates
+
+STATE_OFF = "0"
+STATE_ON = "255"
+STATE_STREAM = "50"
+STATE_PLAY = "100"
+STATE_STOP = "150"
+STATE_PAUSE = "200"
 
 class AgoMopidy(agoclient.AgoApp):
     """AgoControl device for the Mopidy music player"""
@@ -33,38 +43,49 @@ class AgoMopidy(agoclient.AgoApp):
     def message_handler(self, internalid, content):
         """The messagehandler."""
         if "command" in content:
+            self.internalid = internalid
+            # TODO: Move thread start here
             player = self.players[internalid]["mopidy"]
             if content["command"] == "on":
                 self.log.debug("switching on: %s", internalid)
                 # TODO Add ON command
-                self.connection.emit_event(internalid, "event.device.statechanged", "255", "")
+                self.connection.emit_event(internalid, "event.device.statechanged", STATE_ON, "")
+                self.players[internalid]["mopidy"].playingstate = RunningStates.On
+                self.emit_media_info(internalid)
 
             elif content["command"] == "off":
                 self.log.debug("switching off: %s", internalid)
                 # TODO Add OFF command
-                self.connection.emit_event(internalid, "event.device.statechanged", "0", "")
+                self.connection.emit_event(internalid, "event.device.statechanged", STATE_OFF, "")
+                self.players[internalid]["mopidy"].playingstate = RunningStates.Off
 
             elif content["command"] == "play":
                 self.log.debug("Start playing: player %s", internalid)
-                # self.players[internalid]["mopidy"].Play()
+                self.connection.emit_event(internalid, "event.device.statechanged", STATE_PLAY, "")
                 player.Play()
-                #    "event.device.statechanged", "255", "")
+                self.emit_media_info(internalid)
+                self.state = "playing"
             elif content["command"] == "pause":
                 self.log.debug("Pause player %s", internalid)
-                # self.players[internalid]["mopidy"].Pause()
+                self.connection.emit_event(internalid, "event.device.statechanged", STATE_PAUSE, "")
                 player.Pause()
-                #    "event.device.statechanged", "255", "")
+                self.emit_media_info(internalid)
+                self.state = "paused"
+            elif content["command"] == "stop":
+                self.log.debug("Stoping playing: player %s", internalid)
+                self.connection.emit_event(internalid, "event.device.statechanged", STATE_STOP, "")
+                player.Stop()
+                self.emit_media_info(internalid)
+                self.state = "stopped"
 
             elif content["command"] == "next":
                 self.log.debug("Next track. Player %s", internalid)
-                # self.players[internalid]["mopidy"].NextTrack()
                 player.NextTrack()
-                #    "event.device.statechanged", "255", "")
+                self.emit_media_info(internalid)
             elif content["command"] == "previous":
                 self.log.debug("Previous track. Player %s", internalid)
-                # self.players[internalid]["mopidy"].PreviousTrack()
                 player.PreviousTrack()
-                #    "event.device.statechanged", "255", "")
+                self.emit_media_info(internalid)
 
             elif content["command"] == "setvolume":
                 self.log.debug("Set volume for player %s", internalid)
@@ -76,8 +97,6 @@ class AgoMopidy(agoclient.AgoApp):
                     self.log.info('Missing parameter "volume" to command SETVOLUME. Player=%s", internalid')
                     return False
 
-                # "event.device.statechanged", "255", "")
-
             elif content["command"] == "mediainfos":
                 infos = player.GetCurrentTrackInfo()
                 self.log.info("Requested to get MediaInfo. Player %s", internalid) # TODO: revert to debug
@@ -85,82 +104,90 @@ class AgoMopidy(agoclient.AgoApp):
 
     def setup_app(self):
         self.connection.add_handler(self.message_handler)
-
-        # If we want to use the custom command line argument, we look at
-        # self.args:
-        if self.args.test:
-            self.log.info("Test argument was set")
-        else:
-            self.log.warning("Test argument was NOT set")
+        self.internalid = None
 
         self.players = {}
-        players = self.get_config_option("Players", "[none]")
+        playerlist = self.get_config_option("Players", "[none]")
 
-        for p in players.split(","):
+
+        if ',' not in playerlist:
+            playerlist += ','
+
+        print "playerlist=" + playerlist
+
+        for p in playerlist.split(","):
             # TODO: remove spaces from p?
-            self.log.debug('Player ' + p + ' found')
-            host = self.get_config_option("host", "127.0.0.1", section=p, app='mopidy')
-            port = self.get_config_option("port", "6680", section=p, app='mopidy')
-            self.connection.add_device(p, "mopidy")
-            self.log.info("Mopidy player %s added. host=%s, port=%s", p, host, port)
+            if len(p) > 0:
+                print "p=" + p
+                self.log.debug('Player ' + p + ' found')
+                host = self.get_config_option("host", "127.0.0.1", section=p, app='mopidy')
+                port = self.get_config_option("port", "6680", section=p, app='mopidy')
+                self.connection.add_device(p, "mopidy")
+                self.log.info("Mopidy player %s added. host=%s, port=%s", p, host, port)
 
-            mop = Mopidy(host, port)
-            if mop.connected:
-                self.log.info("Mopidy player %s connected.", p)
-            else:
-                self.log.info("Mopidy player %s NOT connected.", p)
+                q = Queue.Queue()
 
-            player = {
-                "name"   : p,
-                "host"   : host,
-                "port"   : port,
-                "mopidy" : mop
-            }
-            self.players[p] = player
-            self.emit_media_info(p)
-            self.log.info("Now playing: %s - %s - %s ", self.players[p]["mopidy"].TrackInfo["artist"],
-                          self.players[p]["mopidy"].TrackInfo["album"],
-                          self.players[p]["mopidy"].TrackInfo["title"])  #TODO: change to debug
+                mop = Mopidy(host, port, q)
+                if mop.connected:
+                    self.log.info("Mopidy player %s connected.", p)
+                else:
+                    self.log.info("Mopidy player %s NOT connected.", p)
 
-            BACKGROUND = MediaInfoCollector(self, mop)
-            BACKGROUND.setDaemon(True)
-            BACKGROUND.start()
+                player = {
+                    "name"   : p,
+                    "host"   : host,
+                    "port"   : port,
+                    "mopidy" : mop,
+                    "queue"  : q
+                }
+                self.players[p] = player
+                self.emit_media_info(p)
+                self.log.info("Now playing: %s - %s - %s ", self.players[p]["mopidy"].TrackInfo["artist"],
+                              self.players[p]["mopidy"].TrackInfo["album"],
+                              self.players[p]["mopidy"].TrackInfo["title"])  #TODO: change to debug
+
+                if mop.runningstate == RunningStates.On:
+                    self.connection.emit_event(p, "event.device.statechanged", STATE_ON, "")
+                else:
+                    self.connection.emit_event(p, "event.device.statechanged", STATE_OFF, "")
+
+                BACKGROUND = MediaInfoCollector(self, p, q)
+                BACKGROUND.setDaemon(True)
+                BACKGROUND.start()
 
     def app_cleanup(self):
         # When our app is about to shutdown, we should clean up any resources we've
         # allocated in app_setup. This is done here.
-        # In this example, we do not have any resources..
         pass
 
     def emit_media_info(self, player):
-        self.log.info('emit MEDIAINFO')
+        self.log.info('emitting MEDIAINFOS')
         TrackInfo = self.players[player]["mopidy"].GetCurrentTrackInfo()
-
-        TrackInfo = {'title': self.players[player]["mopidy"].TrackInfo["title"],
-                     'album': self.players[player]["mopidy"].TrackInfo["album"],
-                     'artist': self.players[player]["mopidy"].TrackInfo["artist"],
-                     'cover': None}
-
-        # TrackInfo = {'title': player.TrackInfo["title"],
-        #             'album': player.TrackInfo["album"],
-        #             'artist': player.TrackInfo["artist"],
-        #             'cover': None}
-
         self.connection.emit_event_raw(player, "event.device.mediainfos", TrackInfo)
-
 
 class MediaInfoCollector(threading.Thread):
     """Class driving a thread to collect media info."""
-    def __init__(self, app, player):
-        threading.Thread.__init__(self)
+    # TODO: Replace with event monitoring via Mopidy WebSocket API
+    def __init__(self, app, player, q):
+        super(MediaInfoCollector, self).__init__()
         self.app = app
         self.player = player
+        self.q = q
+        self.stoprequest = threading.Event()
+
+    def join(self, timeout=None):
+        """Shutdown thread"""
+        self.stoprequest.set()
+        super(MediaInfoCollector, self).join(timeout)
 
     def run(self):
-        # TODO: Add a proper shutdown..
-        # self.player.GetCurrentTrackInfo()
-        # self.app.emit_media_info(self.player)
-        time.sleep(30)
+        while not self.stoprequest.isSet():
+            try:
+                mediainfo = self.q.get(True, 0.5)
+                #print "MediaCollector: mediainfo=" + str(mediainfo)
+                self.app.connection.emit_event_raw(self.player, "event.device.mediainfos", mediainfo)
+            except Queue.Empty:
+                continue
 
 
 if __name__ == "__main__":
--- devices/mopidy/mopidy.py
+++ devices/mopidy/mopidy.py
@@ -1,25 +1,153 @@
 import requests
 import json
+import websocket
+import threading, Queue
+import time
+
+#from enum import Enum
+
+#class RunningStates(Enum):
+class RunningStates():
+    On = 1
+    Off = 2
+
+class PlayingStates():
+    Unknown = 0
+    Playing = 1
+    Paused = 2
+    Stopped = 3
+    #Streaming = 4
+
 
 class Mopidy():
     """Class to access the Mopidy JSON RPC 2.0 API """
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, q=None):
         self.host = host
         self.port = port
         self.url = "http://" + self.host + ":" +  self.port + "/mopidy/rpc"
+        self.wsurl  = "ws://" + self.host + ":" + self.port + "/mopidy/ws"
         self.headers = {'content-type': 'application/json'}
         self.version = self.GetVersion()
         self.TrackInfo = {}
         self.TrackInfo = {'title': 'None', 'album': 'None', 'artist': 'None', 'cover': None} #TODO: Get cover as b64
+        self.runningstate = RunningStates.Off
+        self.playingstate = PlayingStates.Unknown
+        self.connected = False
+        self.q = q
 
         if self.version == None:
             self.connected = False
-            #self.TrackInfo = self.GetCurrentTrackInfo() # Will fail, calling just to get the TrackInfo filled in
             #print "Error communicating with the Mopidy player"
         else:
             self.connected = True
+            self.setRunningState(RunningStates.On)
             self.TrackInfo = self.GetCurrentTrackInfo()
+
+        if self.connected:
+            websocket.enableTrace(True) # TODO: Remove
+            ws = websocket.WebSocketApp(self.wsurl, on_message=self.OnMessage, on_error=self.OnError, on_close = self.OnClose)
+            if ws:
+                print "WS ok"
+            else:
+                print "WS no OK"
+            wst = threading.Thread(target=ws.run_forever)
+            wst.daemon = True
+            wst.start()
+            print "1"
+
+
+            conn_timeout = 5
+
+            while not ws.sock.connected and conn_timeout:
+            #if not ws.sock.connected:
+                print "not yet connected"
+                time.sleep(1)
+                conn_timeout -= 1
+
+            # self.send(ws, "core.get_version")
+
+    def OnMessage(self, ws, message):
+        #print message
+        result = json.loads(message)
+        #print result
+        #Response from Mopidy when playing from Spotify
+        #{"tl_track":
+        #  {"track":
+        #    {"album":
+        #       {"date": "2011",
+        #        "__model__": "Album",
+        #        "artists":
+        #            [{"__model__": "Artist",
+        #              "name": "Various Artists",
+        #              "uri": "spotify:artist:0LyfQWJT6nXafLPZqxe9Of"}],
+        #        "name": "Sucker Punch",
+        #        "uri": "spotify:album:0pdTBKhW8Du8Iher1xi8Gb"},
+        #     "__model__": "Track",
+        #     "name": "Sweet Dreams (Are Made Of This) - Sucker Punch: Original Motion Picture Soundtrack",
+        #     "disc_no": 0,
+        #     "uri": "spotify:track:0cBTbpSTj66GXHBE4eJWdv",
+        #     "length": 319000,
+        #     "track_no": 1,
+        #     "artists": [
+        #        {"__model__": "Artist",
+        #         "name": "Dave Stewart",
+        #         "uri": "spotify:artist:7gcCQIlkkfbul5Mt0jBQkg"},
+        #        {"__model__": "Artist",
+        #         "name": "Emily Browning",
+        #         "uri": "spotify:artist:0ncLUhzYnidjOPYR2DPd7d"},
+        #        {"__model__": "Artist",
+        #         "name": "Annie Lennox",
+        #         "uri": "spotify:artist:5MspMQqdVbdwP6ax3GXqum"}],
+        #      "date": "2011",
+        #      "bitrate": 320},
+        #      "__model__": "TlTrack",
+        #      "tlid": 68},
+        #"event": "track_playback_started"}
+
+        if u'tl_track' in result and u'track' in result[u'tl_track']:
+            tr = result[u'tl_track'][u'track']
+            print "tr - " + str(tr)
+            # print 'date=' + result[u'album'][u'date']
+            if u'album' in tr:
+                album = tr[u'album'][u'name']
+            if u'artists' in tr[u'album']:
+                artist = tr[u'album'][u'artists'][0][u'name']
+            track = tr[u'name']
+
+            print "artist=" + artist
+            print 'album=' + album
+            print "track=" + track
+
+            if self.q != None:
+                TrackInfo = {'title': track, 'album': album, 'artist': artist, 'cover': None}  # TODO: Get cover as b64
+                self.q.put (TrackInfo)
+
+    def OnClose(self, ws):
+        print "WS close received"
+
+    def OnError(self, ws, error):
+        print "WS error received - " + str(error)
+
+    def send(self, ws, method):
+        payload = '{\
+        "method": "' + method + '",\
+        "jsonrpc": "2.0",\
+        "params": {},\
+        "id": 1\
+        }'
+
+        print  payload
+
+        ws.send(payload)
+        print "Sent"
+
+
+    def setRunningState (self, runningstate):
+        self.runningstate = runningstate
+
+    def setPlayingState(self, playingstate):
+        self.playingtate = playingstate
 
     def CallMopidy(self, method, parm1=None, val1=None):
         """Call Mopidy JSON RPC API"""
@@ -46,10 +174,10 @@ class Mopidy():
                 #print response
                 return response["result"]
         except requests.exceptions.ConnectionError:
+            self.connected = False
             #print response #requests.exceptions.ConnectionError
             #TODO: Add logging
             #TODO: figure out how to set self.connected = False
-
             return None
 
     def GetVersion(self):
@@ -67,20 +195,28 @@ class Mopidy():
     def Pause(self):
         """Pause the player"""
         result = self.CallMopidy("core.playback.pause")
+        self.setPlayingState(PlayingStates.Paused)
         return True #TODO: Check playing state and base return on actuals
 
     def Play(self):
-        """Send a Play command to Mopidy. If a track is playinjg, it will be played from the begining"""
+        """Send a Play command to Mopidy. If a track is playing, it will be played from the begining"""
         result = self.CallMopidy("core.playback.play")
+        self.setPlayingState(PlayingStates.Playing)
+        return True #TODO: Check playing state and base return on actuals
+
+    def Stop(self):
+        """Send a Stop command to Mopidy."""
+        result = self.CallMopidy("core.playback.stop")
+        self.setPlayingState(PlayingStates.Stopped)
         return True #TODO: Check playing state and base return on actuals
 
     def NextTrack(self):
-        """Send a Play command to Mopidy. If a track is playinjg, it will be played from the begining"""
+        """Send a NextTrack command to Mopidy."""
         result = self.CallMopidy("core.playback.next")
         return True #TODO: Check playing state and base return on actuals
 
     def PreviousTrack(self):
-        """Send a Play command to Mopidy. If a track is playinjg, it will be played from the begining"""
+        """Send a PreviousTrack command to Mopidy."""
         result = self.CallMopidy("core.playback.previous")
         return True #TODO: Check playing state and base return on actuals
 
@@ -94,10 +230,14 @@ class Mopidy():
     def GetState(self):
         """Get playing state from Mopidy"""
         result = self.CallMopidy("core.playback.get_state")
+        if 'playing' in result:
+            self.setPlayingState(PlayingStates.Playing)
+        elif 'paused' in result:
+            self.setPlayingState(PlayingStates.Paused)
         return result
 
     def GetCurrentTrackInfo(self):
-        """Send a Play command to Mopidy. If a track is playing, it will be played from the begining"""
+        """Get info on currently loaded track in Mopidy."""
         # TODO: Look into handling trackinfo for streamed content
 
         artist = "<none>"
@@ -119,9 +259,9 @@ class Mopidy():
         #print 'album=' + album
         #print "track=" + track
 
-        TrackInfo = {'title': track, 'album': album, 'artist': artist, 'cover': None} #TODO: Get cover as b64
+        self.TrackInfo = {'title': track, 'album': album, 'artist': artist, 'cover': None} #TODO: Get cover as b64
 
-        return TrackInfo
+        return self.TrackInfo
 
 
 if __name__ == "__main__":
@@ -134,8 +274,13 @@ if __name__ == "__main__":
     a.Play()
     print a.GetState()
     a.GetCurrentTrackInfo()
+    print "Next track"
+    a.NextTrack()
+    a.GetCurrentTrackInfo()
+    print "Next track"
+    a.NextTrack()
+    a.GetCurrentTrackInfo()
     a.SetVolume(75)
 
-
-
-
+    while 1==1:
+        time.sleep(5)
